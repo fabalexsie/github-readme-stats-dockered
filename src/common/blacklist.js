08c3664d7028a8f24787d51a3fd2ccfe92e989b1
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-12-08 23:44:11 +0100

    Add whitelist via env WHITE_LISTED_USERS

--- src/common/blacklist.js
+++ src/common/blacklist.js
@@ -1,4 +1,15 @@
-const blacklist = ["renovate-bot", "technote-space", "sw-yx"];
+const blacklist = {
+  includes: (username) => {
+    if ("WHITE_LISTED_USERS" in process.env) {
+      // turn blacklist into whitelist
+      return !process.env.WHITE_LISTED_USERS.split(",").includes(username);
+    } else {
+      // default blacklist
+      return ["renovate-bot", "technote-space", "sw-yx"].includes(username);
+      ;
+    }
+  }
+};
 
 export { blacklist };
 export default blacklist;
